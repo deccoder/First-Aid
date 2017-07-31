a1930967a4568c06acf73551c182e8c7139accff
deccoder <declanbrophy@gmail.com>
2017-07-31 13:09:44 +0100

    Add and view functionality for database

--- app/src/main/java/declanbrophy/firstaid/voiceToText.java
+++ app/src/main/java/declanbrophy/firstaid/voiceToText.java
@@ -13,11 +13,15 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import static declanbrophy.firstaid.R.id.btnAdd;
+import static declanbrophy.firstaid.R.id.voiceEntry;
+
 public class voiceToText extends AppCompatActivity {
     //Declare and create contents of user interface
     private Button speakNow;
     private TextView showVoiceText;
     private final int REQ_CODE_SPEECH_OUTPUT = 143;
+    DatabaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,42 @@ public class voiceToText extends AppCompatActivity {
         //Initialize functionality of user interface
         speakNow = (Button) findViewById(R.id.button);
         showVoiceText = (TextView) findViewById(R.id.showVoiceOutput);
+        Button btnAdd = (Button) findViewById(R.id.btnAdd);
+        Button btnView = (Button) findViewById(R.id.btnView);
+        myDB = new DatabaseHelper(this);
         //Functionality of button to allow user to make voice entry
+        /*btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = TextView.getText.toString();
+                if (TextView.length !=0) {
+                    AddData(newEntry);
+                    TextView.setText("");
+                }else {
+                    Toast.makeText(voiceToText.this, "You must put something in the text field", Toast.LENGTH_LONG).show();
+                }
+            }
+            public void addData(String newEntry) {
+                boolean insertData = myDB.addData(newEntry);
+
+                if (insertData == true) {
+                    Toast.makeText(voiceToText.this, "Data entered successfully", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(voiceToText.this, "Data Entry Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        */
+
+
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(voiceToText.this,LogEntryList.class);
+                startActivity(intent);
+            }
+        });
         speakNow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +74,7 @@ public class voiceToText extends AppCompatActivity {
             }
         });
     }
+
     //Method to recognize speech being said
     private void showVoiceText () {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
